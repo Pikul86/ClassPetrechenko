3d0be0ab56e73e73e72e95bebb8582af54d4b9b9
IURII <pikuliurii@gmail.com>
2022-04-20 20:45:28 +0200

    add fail практика с функциями

--- js/filmObj.js
+++ js/filmObj.js
@@ -10,14 +10,27 @@ let personalMovieDB = {
 };
 
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
- 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {  //null - нажали отмену, '' - пользователь нажал ок и строка пустая
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--; // Возвращает итерацию цикла на один шаг назад, если пользователь выше в условии выполнил что-то не так
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
